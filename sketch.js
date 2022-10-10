ba8ec28bd1500120e5a314c3adbf299ee81a45f9
Sandy Lee <sl30724@gmail.com>
2022-10-10 16:17:42 -0600

    updated sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 /*
-the data source: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0260566
+Data source: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0260566
+Code reference: https://www.youtube.com/watch?v=ZeVbsZQiHdU
 */
 
 let table;
@@ -8,12 +9,35 @@ let pubYear = [];
 let characterGender = [];
 let genre = [];
 let characterType = [];
-let maleCount2019a = 0;
-let femaleCount2019a = 0;
-let maleCount2019b = 0;
-let femaleCount2019b = 0;
-let maleCount2018 = 0;
-let femaleCount2018 = 0;
+let boxSize = 300;
+// test
+let maleCountTest = 0;
+let femaleCountTest = 0;
+// fiction & human
+let fMaleCount = 0;
+let fFemaleCount = 0;
+let fMaleCountArr = [];
+let fFemaleCountArr = [];
+let fRatioArr = [];
+// fiction & non-human
+let nfMaleCount = 0;
+let nfFemaleCount = 0;
+let nfMaleCountArr = [];
+let nfFemaleCountArr = [];
+let nfRatioArr = [];
+// non-fiction & human
+let hMaleCount = 0;
+let hFemaleCount = 0;
+let hMaleCountArr = [];
+let hFemaleCountArr = [];
+let hRatioArr = [];
+// non-fiction & non-human
+let nhMaleCount = 0;
+let nhFemaleCount = 0;
+let nhMaleCountArr = [];
+let nhFemaleCountArr = [];
+let nhRatioArr = [];
+
 
 function preload() {
   table = loadTable("dataset.csv", "csv", "header")
@@ -44,10 +68,13 @@ function setup() {
     characterType[i] = table.getString(i, 8);
   }
   // minYear: 1960, maxYear: 2019
-  // yearMinMax();
-  genderRatio2019a();
-  genderRatio2019b();
-  genderRatio2018();
+  genreTypeGenderRatio();
+  characterTypeGenderRatio();
+  console.log(nhMaleCountArr);
+  console.log(nhFemaleCountArr);
+  console.log(nhRatioArr);
+  //genderRatioTest();
+  ratioMinMax();
 }
 
 function draw() {
@@ -55,8 +82,11 @@ function draw() {
   noFill();
   stroke("black");
   strokeWeight(0.5);
-  let boxSize = 400;
   box(boxSize);
+  push();
+  translate(-width/2, -height/2, -boxSize/2);
+  mainGraph();
+  pop();
 }
 
 function mainGraph() {
@@ -64,66 +94,151 @@ function mainGraph() {
   // y - gender ratio
   // z - genre & character type
 
+  // line 1: fRatioArr
+  let gapx = boxSize / (fRatioArr.length-1);
+  let gapz = boxSize / 5;
+  for (let i = 0; i <= fRatioArr.length; i++) {
+    let x = width / 2 - boxSize / 2 + gapx * (i);
+    let y = map(fRatioArr[i], 0, 2.5, height / 2 + boxSize / 2, height / 2 - boxSize / 2, true);
+    let z = gapz;
+    // let size = map(fRatioArr[i], 0, 2.5, 5, 20);
+    strokeWeight(5);
+    stroke("blue");
+    point(x, y, z);
+  }
+  // line 2: nfRatioArr from 33
+  for (let i = 33; i <= nfRatioArr.length; i++) {
+    let x = width / 2 - boxSize / 2 + gapx * (i);
+    let y = map(nfRatioArr[i], 0, 2.5, height / 2 + boxSize / 2, height / 2 - boxSize / 2, true);
+    let z = gapz * 2;
+    // let size = map(fRatioArr[i], 0, 2.5, 5, 20);
+    strokeWeight(5);
+    stroke("green");
+    point(x, y, z);
+  }
+  // line 3: hRatioArr from 1
+  for (let i = 1; i <= hRatioArr.length; i++) {
+    let x = width / 2 - boxSize / 2 + gapx * (i);
+    let y = map(hRatioArr[i], 0, 9, height / 2 + boxSize / 2, height / 2 - boxSize / 2, true);
+    let z = gapz * 3;
+    // let size = map(fRatioArr[i], 0, 2.5, 5, 20);
+    strokeWeight(5);
+    stroke("yellow");
+    point(x, y, z);
+  }
+  // line 4: nhRatioArr from 22
+  for (let i = 22; i <= nhRatioArr.length; i++) {
+    let x = width / 2 - boxSize / 2 + gapx * (i);
+    let y = map(nhRatioArr[i], 0, 9, height / 2 + boxSize / 2, height / 2 - boxSize / 2, true);
+    let z = gapz * 4;
+    // let size = map(fRatioArr[i], 0, 2.5, 5, 20);
+    strokeWeight(5);
+    stroke("orange");
+    point(x, y, z);
+  }
 }
 
-function genderRatio2019a() {
-  for (let i = 0; i < numRows; i++) {
-    if (pubYear[i] == 2019 && genre[i] == "fiction" && characterType[i] == "human") {
-      if (characterGender[i] == "male") {
-        maleCount2019a++;
-      } else if (characterGender[i] == "female") {
-        femaleCount2019a++;
+function genreTypeGenderRatio() {
+  for (let ii = 1960; ii < 2020; ii++) {
+    for (let i = 0; i < numRows; i++) {
+      if (pubYear[i] == ii && genre[i] == "fiction") {
+        if (characterGender[i] == "male") {
+          // count the male number and female number of the year
+          fMaleCount++;
+          fMaleCountArr[ii - 1960] = fMaleCount;
+        } else if (characterGender[i] == "female") {
+          fFemaleCount++;
+          fFemaleCountArr[ii - 1960] = fFemaleCount;
+        }
+        fRatioArr[ii - 1960] = fFemaleCount / fMaleCount;
+      } else if (pubYear[i] == ii && genre[i] == "non-fiction") {
+        if (characterGender[i] == "male") {
+          // count the male number and female number of the year
+          nfMaleCount++;
+          nfMaleCountArr[ii - 1960] = nfMaleCount;
+        } else if (characterGender[i] == "female") {
+          nfFemaleCount++;
+          nfFemaleCountArr[ii - 1960] = nfFemaleCount;
+        }
+        nfRatioArr[ii - 1960] = nfFemaleCount / nfMaleCount;
       }
     }
+    fMaleCount = fFemaleCount = 0;
+    nfMaleCount = nfFemaleCount = 0;
   }
-  let ratio2019a = maleCount2019a / femaleCount2019a;
-  print(ratio2019a);
+  // print(fMaleCountArr[58]);
+  // print(fFemaleCountArr[58]);
+  // print(fRatioArr);
 }
 
-function genderRatio2019b() {
-  for (let i = 0; i < numRows; i++) {
-    if (pubYear[i] == 2019 && genre[i] == "non-fiction" && characterType[i] == "human") {
-      if (characterGender[i] == "male") {
-        maleCount2019b++;
-      } else if (characterGender[i] == "female") {
-        femaleCount2019b++;
+function characterTypeGenderRatio() {
+  for (let ii = 1960; ii < 2020; ii++) {
+    for (let i = 0; i < numRows; i++) {
+      if (pubYear[i] == ii && characterType[i] == "human") {
+        if (characterGender[i] == "male") {
+          // count the male number and female number of the year
+          hMaleCount++;
+          hMaleCountArr[ii - 1960] = hMaleCount;
+        } else if (characterGender[i] == "female") {
+          hFemaleCount++;
+          hFemaleCountArr[ii - 1960] = hFemaleCount;
+        }
+        hRatioArr[ii - 1960] = hFemaleCount / hMaleCount;
+      } else if (pubYear[i] == ii && characterType[i] == "non-human") {
+        if (characterGender[i] == "male") {
+          // count the male number and female number of the year
+          nhMaleCount++;
+          nhMaleCountArr[ii - 1960] = nhMaleCount;
+        } else if (characterGender[i] == "female") {
+          nhFemaleCount++;
+          nhFemaleCountArr[ii - 1960] = nhFemaleCount;
+        }
+        nhRatioArr[ii - 1960] = nhFemaleCount / nhMaleCount;
       }
     }
+    hMaleCount = hFemaleCount = 0;
+    nhMaleCount = nhFemaleCount = 0;
   }
-  let ratio2019b = maleCount2019b / femaleCount2019b;
-  print(ratio2019b);
+  // print(nhMaleCountArr[58]);
+  // print(nhFemaleCountArr[58]);
+  // print(nhRatioArr[58]);
 }
 
-function genderRatio2018() {
-  for (let i = 0; i < numRows; i++) {
-    if (pubYear[i] == 2018 && genre[i] == "fiction" && characterType[i] == "human") {
-      if (characterGender[i] == "male") {
-        maleCount2018++;
-      } else if (characterGender[i] == "female") {
-        femaleCount2018++;
-      }
-    }
-  }
-  let ratio2018 = maleCount2018 / femaleCount2018;
-  print(ratio2018);
-}
-// find max and min year in data
-// let yearMin, yearMax = 0;
-// function yearMinMax() {
+// test if the loop statement returns correctly
+// function genderRatioTest() {
 //   for (let i = 0; i < numRows; i++) {
-//     if (table.getNum(i, 3) > yearMax) {
-//       yearMax = table.getNum(i, 3);
+//     if (pubYear[i] == 2018 && characterType[i] == "non-human") {
+//       if (characterGender[i] == "male") {
+//         maleCountTest++;
+//       } else if (characterGender[i] == "female") {
+//         femaleCountTest++;
+//       }
 //     }
 //   }
-
-//   yearMin = yearMax;
-
-//   for (let i = 0; i < numRows; i++) {
-//     if (table.getNum(i, 3) < yearMin) {
-//       yearMin = table.getNum(i, 3);
-//     }
-//   }
-
-//   print(`yearMax ${yearMax}, yearMin ${yearMin}`);
-
+//   let ratioTest = femaleCountTest / maleCountTest;
+//   print(maleCountTest);
+//   print(femaleCountTest);
+//   print(ratioTest);
 // }
+
+
+// find max and min value in data
+let ratioMin, ratioMax = 0;
+function ratioMinMax() {
+  for (let i = 0; i < 60; i++) {
+    if (nhRatioArr[i] > ratioMax) {
+      ratioMax = nhRatioArr[i];
+    }
+  }
+
+  ratioMin = ratioMax;
+
+  for (let i = 0; i < 60; i++) {
+    if (nhRatioArr[i] < ratioMin) {
+      ratioMin = nhRatioArr[i];
+    }
+  }
+
+  print(`ratioMax ${ratioMax}, ratioMin ${ratioMin}`);
+
+}
